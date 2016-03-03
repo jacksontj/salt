52fcfe4e0653415246d532750b794f4527410fbb
Thomas Jackson <jacksontj.89@gmail.com>
2016-03-03 09:18:18 -0800

    Remove unnecessary comment

--- salt/master.py
+++ salt/master.py
@@ -1259,7 +1259,6 @@ class AESFuncs(object):
         load['grains']['id'] = load['id']
 
         pillar_dirs = {}
-#        pillar = salt.pillar.Pillar(
         pillar = salt.pillar.get_pillar(
             self.opts,
             load['grains'],
